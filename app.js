080b33db8fd977da2be9a77b70cae1333a15e292
vfrtist <137813892+vfrtist@users.noreply.github.com>
2023-08-28 21:08:53 -0400

    Initial upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// const axios = require('axios').default;
+const Key = 'owyoRxVRwY5VFh04MPfIHg==FDVCLpEIG7YcTz7H'
+const main = document.querySelector('main');
+const headers = { headers: { 'X-Api-Key': Key } };
+const url = 'https://api.api-ninjas.com/v1/randomword';
+const button = document.querySelector('button');
+const header = document.querySelector('h1');
+
+button.addEventListener('click', () => {
+    getNewWord();
+})
+
+function titleCase(str) {
+    return str.toLowerCase().split(' ').map(function (word) {
+        return word.replace(word[0], word[0].toUpperCase());
+    }).join(' ');
+}
+
+const getNewWord = async () => {
+    try {
+        const res = await axios.get(url, headers);
+        // console.log(res.data);
+        newWord = `${res.data.word} 2.0`
+        header.innerText = titleCase(newWord);
+    } catch (e) {
+        console.log('error', e);
+    }
+}
